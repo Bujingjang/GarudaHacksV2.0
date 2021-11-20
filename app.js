85fde680f14fc5aa54b118c7a98c342b69cbc516
Daniel Dewanto <daniel.dewanto1111@gmail.com>
2021-11-20 13:56:29 +0700

    creating fixed template login page

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
-app.use(express.static("images"))
-app.use(express.static("styles"))
-app.use(express.static("views"))
-
-
+app.use(express.static("images"));
+app.use(express.static("styles"));
+app.use(express.static("views"));
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, './views/homePage/Home.html'));
+    res.sendFile(path.join(__dirname, './views/Home.html'));
+    //res.send("test");
 });
 
-app.listen(8080,()=>{
+app.listen(8088,()=>{
     console.log("Connected to server");
 });
