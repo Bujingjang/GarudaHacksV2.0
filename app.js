3c6c70be773e39d6739ea885a99e0b0d7ee5e04e
reyvb24 <reyvb24@connect.hku.hk>
2021-11-20 15:59:56 +0800

    fixed firebase

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const app2 = express();
 const bodyParser = require("body-parser");
 const firebaseAdmin = require("firebase-admin");
-const firebaseFunctions = require("firebase-functions");
+const serviceAccount = require("./garudahacks-f6ce2-firebase-adminsdk-pq2va-c79c219345.json");
 
-app.use('/api/v1', app2);
 app.use(bodyParser.json());
 app.use(express.static("images"));
 app.use(express.static("styles"));
 app.use(express.static("views"));
 app.use(express.static(__dirname + '/public'));
 
-firebaseAdmin.initializeApp(firebaseFunctions.config().firebase);
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+});
 
 const db = firebaseAdmin.firestore();
 const userCollection = 'users';
-const webApi = firebaseFunctions.https.onRequest(app);
 
 app.get('/', function(req, res){
     res.render(path.join(__dirname, "./views/Home.ejs"));
