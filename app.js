2e49f1abf2e74fda44f9a9423bc7fb297214c702
reyvb24 <reyvb24@connect.hku.hk>
2021-11-20 15:46:46 +0800

    halfway through connecting with firebase

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const app2 = express();
+const bodyParser = require("body-parser");
+const firebaseAdmin = require("firebase-admin");
+const firebaseFunctions = require("firebase-functions");
 
-
+app.use('/api/v1', app2);
+app.use(bodyParser.json());
 app.use(express.static("images"));
 app.use(express.static("styles"));
 app.use(express.static("views"));
 app.use(express.static(__dirname + '/public'));
+
+firebaseAdmin.initializeApp(firebaseFunctions.config().firebase);
+
+const db = firebaseAdmin.firestore();
+const userCollection = 'users';
+const webApi = firebaseFunctions.https.onRequest(app);
 
 app.get('/', function(req, res){
     res.render(path.join(__dirname, "./views/Home.ejs"));
