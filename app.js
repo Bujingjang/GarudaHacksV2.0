dd4b5c29f9c00614dca7df4d0f665ca29fa74495
reyvb24 <reyvb24@connect.hku.hk>
2021-11-20 20:50:53 +0800

    basic authentication for registering

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const path = require('path');
 const app = express();
 const bodyParser = require("body-parser");
 const firebaseAdmin = require("firebase-admin");
+const firebaseService = require("firebase-service");
 const serviceAccount = require("./garudahacks-f6ce2-firebase-adminsdk-pq2va-c79c219345.json");
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("images"));
 app.use(express.static("styles"));
@@ -18,6 +21,18 @@ firebaseAdmin.initializeApp({
 const db = firebaseAdmin.firestore();
 const userCollection = 'users';
 
+const getAuthToken = (req, res, next) => {
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'Bearer'
+    ) {
+        req.authToken = req.headers.authorization.split(' ')[1];
+    } else {
+        req.authToken = null;
+    }
+    next();
+};
+
 app.get('/', function(req, res){
     res.render(path.join(__dirname, "./views/Home.ejs"));
     //res.send("test");
@@ -25,21 +40,50 @@ app.get('/', function(req, res){
 
 app.get('/register-influencer', function(req, res) {
     console.log("customer");
-    let anjing = {
-        Public: {
-            name: "ANJING",
-        },
-    };
+    // let anjing = {
+    //     Public: {
+    //         name: "ANJING",
+    //     },
+    // };
     // db.collection("Sample").doc("TIKBAI").set(anjing);
-    res.render(path.join(__dirname, "views/register.ejs"));
+    res.render(path.join(__dirname, "views/register.ejs"), {error:''});
 });
 
-app.post('register-influencer', function(req, res) {
+app.post('/register-influencer', async (req, res) => {
     console.log("posted data for registering influencers");
-    res.redirect();
+    const {
+        displayName,
+        email,
+        password,
+        birthdate,
+        phoneNumber
+    } = req.body;
+    console.log(req.body);
+    const user = await firebaseAdmin.auth().createUser({
+        email,
+        password,
+        birthdate,
+        phoneNumber: phoneNumber,
+        displayName: displayName,
+    }).catch(
+        (err)=> {
+            console.log(err);
+            res.render(path.join(__dirname, "views/register.ejs"), {error: err});
+        }
+    );
+    console.log(user);
+    // const user = await firebaseAdmin.auth().createUserWithEmailAndPassword({
+    //     email:email,
+    //     password:password
+    // }).catch(err=>console.log(err));
+    res.redirect("/");
 });
 
 app.get('/register-employer', function(req, res) {
+    res.render(path.join(__dirname, "views/register.ejs"));
+});
+
+app.get('/home', function(req, res) {
     res.render(path.join(__dirname, "views/register.ejs"));
 });
 
