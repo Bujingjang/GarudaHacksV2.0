462ac7ca175b25dbd8bb5e12d1fb9ed787657db5
reyvb24 <reyvb24@connect.hku.hk>
2021-11-20 15:14:24 +0800

    edited app.js and modified paths

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-let express = require('express');
-let path = require('path');
-let app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
+
 
 app.use(express.static("images"));
 app.use(express.static("styles"));
 app.use(express.static("views"));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, './views/Home.html'));
+    res.render(path.join(__dirname, "./views/Home.ejs"));
     //res.send("test");
 });
 
-app.listen(8088,()=>{
+app.get('/register-influencer', function(req, res) {
+    console.log("customer");
+    res.render(path.join(__dirname, "views/register.ejs"));
+});
+
+app.get('/register-employer', function(req, res) {
+    res.render(path.join(__dirname, "views/register.ejs"));
+});
+
+app.listen(8080,()=>{
     console.log("Connected to server");
 });
