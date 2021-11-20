253db7981502e80aef10f1a16862a255b8c366a8
Daniel Dewanto <daniel.dewanto1111@gmail.com>
2021-11-20 13:44:54 +0700

    homepage finished

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 let express = require('express');
+let path = require('path');
 let app = express();
 
+app.use(express.static("images"))
+app.use(express.static("styles"))
+app.use(express.static("views"))
+
+
+
 app.get('/', function(req, res){
-    res.send("test");
+    res.sendFile(path.join(__dirname, './views/homePage/Home.html'));
 });
 
-app.listen(8080);
+app.listen(8080,()=>{
+    console.log("Connected to server");
+});
